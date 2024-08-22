111db6ddeacb2c6ae137656e18bb0afd6168f2e5
ulock <duongbui2612@gmail.com>
2024-08-22 08:38:16 +0700

    version 1 : haven't store dât

--- code_src2/todos/script.js
+++ code_src2/todos/script.js
@@ -1,17 +1,34 @@
+
+
+
 function add_li()
-{     var value = document.getElementById("input").value; // lấy value từ ô input
+{     if(document.getElementById("input").value != "")
+     {   var value = document.getElementById("input").value; // lấy value từ ô input
       var ul = document.getElementById("list"); // lấy thẻ ul
       
       var li = document.createElement("li"); // tạo thẻ li
+      var checkbox = document.createElement("input"); // tạo thẻ input
+      checkbox.type = "checkbox"; // gán type cho thẻ input
+      checkbox.id = "checkbox"; // gán id cho thẻ input
+      
+      var span = document.createElement("span"); // tạo thẻ span
+      span.contentEditable = "true"; // gán thuộc tính contentEditable cho thẻ span
+      
+      
       var deleteButton = document.createElement("button"); // tạo thẻ button
-      li.textContent = value; // gán value vào thẻ li
       deleteButton.textContent = "Delete"; // gán value vào thẻ button
       deleteButton.className = "delete"; // gán id cho th
-      deleteButton.onclick = function() { li.remove(); deleteButton.remove() }// tạo sự kiện khi click vào button
-      ul.append(li); // thêm thẻ li vào thẻ ul
-      li.append(deleteButton); // thêm thẻ button vào thẻ ul
+    
+        checkbox.onclick = function() { li.style.textDecoration = checkbox.checked ? "line-through" : "none" } // tạo sự kiện khi click vào checkbox
+        span.textContent = value; // gán value vào thẻ span
+        deleteButton.onclick = function() { li.remove(); }// tạo sự kiện khi click vào button
+        ul.appendChild(li); // thêm thẻ li vào thẻ ul
+        li.appendChild(checkbox); // thêm thẻ input vào thẻ ul
+        li.appendChild(span); // thêm thẻ span vào thẻ li
+      li.appendChild(deleteButton); // thêm thẻ button vào thẻ ul
+      document.getElementById("input").value="" 
 
-     
+}
 }
 
 
