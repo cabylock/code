2cfb8050b62f80ef931f4285a4ac8938541fe744
ulock <duongbui2612@gmail.com>
2024-08-31 23:55:48 +0700

    phân hoạch lumoto và  hoare

--- code_src/file_c++/dsa/sort/quick_sort.cpp
+++ code_src/file_c++/dsa/sort/quick_sort.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
-int pivot( int a[] , int l , int r)
+int hoare(int a[], int l, int r )
+{ 
+  int pivot = a[(l+r)/2]; // chọn pivot là phần tử giữa của đoạn
+  int i = l;int j = r;
+  while( i <= j )
+  {
+    while( a[i] < pivot ) i++;// tìm phần tử >= pivot bên trái
+    while( a[j] > pivot ) j--;// tìm phần tử <= pivot bên phải
+    if( i <= j )
+    {
+      swap( a[i], a[j]);//ko quan trọng pivot bị đổi chỗ 
+      i++;
+      j--;
+    }
+  }
+   return i;// vị trí pivot mới -> đoạn [l, i-1] < pivot cũ <= [i, r] 
+}
+
+
+int lomuto( int a[] , int l , int r) // phân hoạch lomuto => pivot = end element
 {  // 1 9 1 3 
    int i = l ;  //i là vị trí đổi chỗ 
    int pivot = a[r]; // chọn pivot là phần tử cuối của đoạn 
@@ -15,6 +34,7 @@ int pivot( int a[] , int l , int r)
       }
    }
    swap( a[ i], a[r]); // đổi pivot vào giữa đoạn
+   
    return i ; // trả về vị trí pivot 
 
 }
@@ -23,19 +43,20 @@ void quicksort( int a[], int l , int r )
 {
    if( l < r ) // l =r có 1 phần tử thì ko cần 
    {  
-      int p = pivot(a, l, r );
+      int p = hoare(a, l, r );
       quicksort( a,l, p-1 ); // nếu cho quicksort(a, l, p) thì khi đoạn có 2 phần tử thì p = vị trí thứ 2 
                            // p không đổi  => lặp vô hạn 
-      quicksort( a, p+1 , r);
+      quicksort( a, p , r);// nếu dùng lomuto => quicksort( a, p+1, r) / quicksort( a, p, r-1) do phần tử pivot đã đúng vị trí
+                           // nếu dùng hoare => quicksort( a, p, r) do pivot chưa đúng vị trí
    }
 
 }
 
 int main()
 {
-   int a[11]= { 1 ,4 ,5 ,2000,6 ,9 ,205 ,1 ,4 ,2,10};
-
-   quicksort( a, 0 , 10 );
+   int a[11]= { 1 , 10 , 4 ,5 , 9 ,19, 8,14, 7,6,8};
+   // int b[4] = { 1, 9, 1, 3};
+   quicksort( a, 0, 10);
    for ( auto i : a )
    {
       cout << i <<" ";
