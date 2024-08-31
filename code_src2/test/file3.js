b14e7b7068af7e891b664fff09f75410f2a5585a
ulock <duongbui2612@gmail.com>
2024-08-31 23:55:55 +0700

    nothing

--- code_src2/test/file3.js
+++ code_src2/test/file3.js
@@ -1,24 +1,21 @@
-
-
-
-// const getdata = require('./file2.js')
-
-// getdata.abc; // Hello World;
-
-let a = 
-{
-   name: 'John',
-   age: 25
-}
-let b = {
-   "name": "John Doe",
-   "age": 30,
-   
- }
-
-console.log(  b.name);
-
-
-
-
-
+function log(target, name, descriptor) {
+    const original = descriptor.value;
+    descriptor.value = function(...args) {
+      console.log(`Calling ${name} with arguments: ${args}`);
+      return original.apply(this, args);
+    };
+    return descriptor;
+  }
+  
+  class Calculator {
+    add(a, b) {
+      return a + b;
+    }
+  }
+  
+  // Áp dụng decorator
+  Object.defineProperty(Calculator.prototype, 'add', log(Calculator.prototype, 'add', Object.getOwnPropertyDescriptor(Calculator.prototype, 'add')));
+  
+  // Tạo instance và sử dụng
+  const calc = new Calculator();
+(calc.add(2, 3)); // Sẽ log: "Calling add with arguments: 2,3" và trả về 5
