36674e5f2202e60e54d1c5f2715df492db5f36d4
ocean0000 <duongbui2612@gmail.com>
2024-10-29 22:30:40 +0700

    nothing

--- code_src/file_c++/test/test4.cpp
+++ code_src/file_c++/test/test4.cpp
@@ -1,21 +1,13 @@
 #include<iostream>
-using  namespace std; 
+using namespace  std; 
 
-void prin1(int * p )
-{
-	cout << *p ;  cout <<" " << p<<endl;  ; 
-}
-void print2( int *& p )
-{
-	cout  << *p << " " <<  p <<endl ;
-}
+
+
 
 int main()
 {
-	int a =  4; 
-	int * p  = & a; 
-	cout << & a <<endl ; 
-	prin1(p); 
-	print2(p);
 
 }
+
+
+
