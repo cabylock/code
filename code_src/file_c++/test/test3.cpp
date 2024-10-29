9899e28594f9ef1f7d2b3029e4cc323cf430ae82
ocean0000 <duongbui2612@gmail.com>
2024-10-29 22:14:04 +0700

    add general folder

--- code_src/file_c++/test/test3.cpp
+++ code_src/file_c++/test/test3.cpp
@@ -1,211 +1,100 @@
 #include<iostream>
 
-using namespace std; 
-int n ;
-void copy_array(int src[], int dest[], int n) {
-    memcpy(dest, src, n * sizeof(int));
-}
+using namespace  std; 
 
-void insert_sort( int a[], int n )
-{  
-   for( int i = 1; i< n ; i++)
-   {
-      int j = i -1; 
-      int key = a[i]; 
-      while( j >= 0  &&  a[j] > key)
-      {
-         a[j+1] =a[j];
-         j--; 
-      }
-      a[j+1] = key ;
-
-
-   }
-   for ( int i = 0 ; i< n; i ++)
-   {
-      cout << a[i] <<" "; 
-   }
-   cout << endl; 
-
-
-}
-
-
-void bubble_sort ( int a[], int n )
+struct node 
 {
-   for ( int i = 0 ; i< n; i ++)
-   {
-      for ( int j  = 0 ;j < n-1; j++)
-      {
-         if ( a[j] > a[j+1]   )
-         {
-            int temp = a[j]; 
-            a[j] = a[j+1];
-            a[j+1] = temp ;
-         }
-      }
-   }
-   for( int i = 0 ; i< n; i++)
-   {
-      cout << a[i] <<" ";
-   }
-   cout << endl; 
+   int data; 
+   node * next; 
+}; 
 
-}
-
-
-void selection_sort ( int a[], int n )
+void   insert(node * root, int data, int index)
 {
-   for ( int i = 0 ; i< n; i++)
-   {  
-      int min_i = i ;
-      for ( int j = i +1; j < n; j ++)
+      if( root == NULL)
       {
-         if( a[j] < a[min_i])
-         {
-            min_i = j ; 
-         }
-      }
-      int temp = a[i]; 
-      a[i] = a[min_i];
-      a[min_i] = temp ; 
-   }
-
-   for ( int i = 0 ; i< n; i++)
-   {
-      cout << a[i] << " ";
-   }
-   cout << endl; 
-}
-
-void merge(int a[], int l , int p , int r )
-{
-   int n1 = p-l+1 ; int n2 = r- p ;
-   int left[ n1] ;
-   int right [n2 ];
-
-   for ( int i = 0 ; i <n1; i++)
-   {
-      left[i] = a[i+l];
-   }
-   for ( int i = 0; i <n2 ; i ++)
-   {
-      right[i] = a[i + p +1] ;
-   }
-   
-   int i = 0 ; int j = 0 ;
-   while( i < n1  && j < n2 )
-   {
-      if( left[i] < right[j])
-      {
-         a[l++] = left[i++];
+         root = new node; 
+         root -> data = data;
+         root -> next = NULL ; 
+         
       }
       else 
       {
-         a[l++] = right[j++];
-      }
-   }
-
-   while( i < n1)
-   {
-      a[l++] =left[i++];
-   }
-   while( j < n2)
-   {
-      a[l++] = right[j++]; 
-   }
-
-
-}
-
-void merge_sort( int a[], int l , int r  )
-{
-      if( r > l )
-      {
-         merge_sort( a, l, (r+l) /2 ); 
-         merge_sort( a, (r+l)/2 + 1 , r );
-         merge( a, l, (r+l)/2 , r ) ;
-      }
-      if( l == 0 && r== n-1)
-      {
-         for ( int i = 0 ; i< n; i ++)
+           index  -- ; 
+           node * p = root; 
+         while( index -- )
          {
-            cout << a[i] << " ";
+            p = p -> next; 
          }
-         cout << endl; 
+         node * temp = p ->next; 
+         p-> next = new node; 
+         p->next ->data = data ; 
+         p->next -> next = temp ; 
+         
+
+
       }
-      
 }
 
-int  lomuto( int a[], int l , int r )
+void add(node * &  root , int data)
 {
-   int key = a[r]; 
-   int i = 0 ; int j = 0 ;
-   while(j < r  )
-   {  
-      if( a[j] < key )
+   node * p = root ; 
+    if( root == NULL)
       {
-
-      int temp = a[i] ; 
-      a[i] =a [j]; 
-      a[j] =temp ; i ++ ;
+         root = new node(); 
+         root -> data = data;
+         root -> next = NULL ; 
+         return ; 
       }
-      j ++ ; 
+   while(  p -> next != NULL)
+   {
+      p = p ->next; 
    }
-   int temp = a[i]; 
-   a[i] = key ; 
-   a[r] =temp ;
+   p ->next =new node; 
+   p->next ->data = data; 
+   p->next -> next = NULL ;
+}
+
+
+void erase( node * & root, int index)
+{
+   node * p  = root; 
+   if( index == 1)
+   {
+      root = root ->next;
+      return  ;
+   }
+   index -- ; 
+   while(  -- index )
+   {
+      p = p ->next; 
+   } 
+   p ->next = p ->next ->next; 
    
-   return i; 
 }
 
-void quick_sort( int a[], int l , int r )
+
+void print_( node  * root )
 {
-   if( l < r )
+   node * p = root  ;  
+   while( p != NULL)
    {
-      int p = lomuto( a, l , r );
-      
-      quick_sort( a, l , p-1); 
-      quick_sort(a, p+1, r ); 
-   }
-   if( l == 0 && r == n-1)
-   {
-      for ( int i =  0 ; i< n ; i ++)
-      {
-         cout << a[i] <<" ";
-      }
-      cout << endl ; 
+      cout << p  ->  data <<" "; 
+      p = p -> next; 
    }
 }
-
 
 
 int main()
 {
-   cin >> n; 
-   int a[n]; 
-   for ( int i = 0 ; i< n; i++)
-   {
-      cin >> a[ i]; 
-   }
-   int temp[n];
-   copy_array(a,temp, n); 
-   cout << "insert_sort " ;  insert_sort(temp,n);
+   node * root = NULL ; 
 
-   copy_array(a,temp, n); 
-   cout <<"bubble_sort " ; bubble_sort( temp,n );
-
-   copy_array(a,temp, n); 
-   cout << "select_sort "; selection_sort( temp,n) ;
-
-   copy_array(a, temp , n ); 
-   cout <<" merge_sort "; merge_sort(temp,0, n-1);
-
-   copy_array( a , temp , n );
-   cout << " quick_sort " ; quick_sort( temp, 0 , n-1 ); 
-
-   copy_array( a, temp , n ); 
-   cout << " radix_sort "; 
-
+   add( root , 2) ; 
+   add ( root , 4);
+   insert( root, 5, 1);
+   insert(root, 7, 1);
+ 
+   print_(root ); 
 
 }
+
+
--- code_src/file_c++/test/test4.cpp
+++ code_src/file_c++/test/test4.cpp
@@ -1,62 +1,21 @@
 #include<iostream>
+using  namespace std; 
 
-using namespace std;
-
-int queu[100];
-int front  = -1 ; 
-int rear = -1 ; 
-
-void push(int x)
-{	
-	if( (rear +1)%100 ==front )
-	{
-		cout<< "full" <<endl; return; 
-	}
-	rear ++;
-	rear= (rear % 100) ; 
-	
-	queu[rear] = x; 
-}
-
-void peek()
-{	
-	if( front == rear)
-	{cout<< "empty" <<endl ; 
-		return ;
-	}
-
-	cout<< queu[front+1]<<endl;
-}
-void pop()
-{	
-	if( rear == front)
-	{	
-		rear = -1;
-		front = -1; 
-		cout<<" empty" << endl; 
-	}
-	front ++ ; 
-	front = (front % 100);
-
-}
-void isempty()
+void prin1(int * p )
 {
-	front == rear ? cout<< "empty" << endl : cout<< "no_empty"<<endl ;
+	cout << *p ;  cout <<" " << p<<endl;  ; 
 }
-
-
+void print2( int *& p )
+{
+	cout  << *p << " " <<  p <<endl ;
+}
 
 int main()
-{	
-	isempty();
-	push(5);
-	push(8); 
-	isempty();
-	peek(); 
-	pop();
+{
+	int a =  4; 
+	int * p  = & a; 
+	cout << & a <<endl ; 
+	prin1(p); 
+	print2(p);
 
-	peek();
 }
-
-
-
