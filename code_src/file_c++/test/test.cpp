44a1c28c07124e306efeecacfdcf1b3e8473981c
ocean0000 <duongbui2612@gmail.com>
2024-10-29 09:04:27 +0700

    nothing

--- code_src/file_c++/test/test.cpp
+++ code_src/file_c++/test/test.cpp
@@ -1,58 +1,73 @@
 #include<iostream>
-#include<stack>
-using namespace std;
+using namespace std; 
 
+int len = 0; 
+int heap[100];
 
-
-int main()
-{
-    int t ; cin >> t; 
-    while( t--)
-    {
-        string s; cin >> s; 
-        stack<char> st; 
-        for( int i = 0; i< s.size(); i++)
-        {
-            if( s[i] == '(' || s[i] == '[' || s[i] == '{')
-            {
-                st.push(s[i]);
-            }
-            else
-            {
-                if( st.empty())
-                {
-                    cout<<"no"<<endl;
-                    break;
-                }
-                else
-                {
-                    if( s[i] == ')' && st.top() == '(')
-                    {
-                        st.pop();
-                    }
-                    else if( s[i] == ']' && st.top() == '[')
-                    {
-                        st.pop();
-                    }
-                    else if( s[i] == '}' && st.top() == '{')
-                    {
-                        st.pop();
-                    }
-                    else
-                    {
-                        cout<<"no"<<endl;
-                        break;
-                    }
-                }
-            }
-        }
-        if( st.empty())
-        {
-            cout<<"yes"<<endl;
-        }
-        else
-        {
-            cout<<"no"<<endl;
-        }
+void swim(int i) {   
+    if (i <= 0) return; 
+    if (heap[i] < heap[(i-1)/2]) {
+        swap(heap[i], heap[(i-1)/2]);
+        swim((i-1)/2);
     }
+}
+
+void sink(int i) {
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    int smallest = i;
+
+    if (left < len && heap[left] < heap[smallest]) {
+        smallest = left;
+    }
+    if (right < len && heap[right] < heap[smallest]) {
+        smallest = right;
+    }
+    if (smallest != i) {
+        swap(heap[i], heap[smallest]);
+        sink(smallest);
+    }
+}
+
+void erase() {
+    swap(heap[0], heap[len-1]);
+    len--;
+    sink(0);
+}
+
+void insert(int data) {
+    heap[len] = data;
+    swim(len);
+    len++;
+}
+
+void print_() {
+    for (int i = 0; i < len; i++) {
+        cout << heap[i] << " ";
+    }
+    cout << endl;
+}
+
+void heapsort() {   
+  
+    while (len > 0) {
+        cout << heap[0] << " "; 
+        erase(); 
+    }
+    // Reset heap length after sorting
+}
+
+int main() {   
+    insert(95);
+    insert(61);
+    insert(83);
+    insert(53);
+    insert(39);
+    insert(72);
+    insert(16);
+    insert(24);
+    insert(48);
+      
+    heapsort();
+    return 0;
 }
--- code_src/file_c++/test/test3.cpp
+++ code_src/file_c++/test/test3.cpp
@@ -1,25 +1,22 @@
 #include<iostream>
-#include<map>
+
 using namespace std; 
 
 
-
+void pr( int * z)
+{
+   ++* z  ; 
+  
+}
 
 int main()
 {
-   map<float,int> a; 
-   
-   for(int i = -20; i<= 20 ; i++)
-   {
-      a[(3/4*i)] ++;
-      a[2*i] ++ ;
-   }
 
-   for( int i = 10; i<=10; i++)
-   {
-      cout<< a[i]
-   }
+   int a = 6; 
+   int *p =&a;
 
+  pr( &p) ; 
 
+   cout << *p ;
 
 }
