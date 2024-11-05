6ade54c6de2d80869968be2b6a133ad69f0e1cc5
ocean0000 <duongbui2612@gmail.com>
2024-11-05 17:23:21 +0700

    use local storage

--- code_src2/todos/script.js
+++ code_src2/todos/script.js
@@ -1,114 +1,105 @@
 
-function log( value, checked)  {     
-      let li = document.createElement('li');
-      let ul = document.createElement('ul');
+let list= return_todo();
+function return_todo()
+{
+      let todo = localStorage.getItem("todo");
+      if(todo)
+      {
+            return JSON.parse(todo);
+      }
+      else
+      {
+            return [];
+      }
 
-      let  checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.id="checkbox";
+}
 
-      let span = document.createElement('span');
+function delete_list(value)
+{
+       list = return_todo();
+      let index = list.findIndex((item) => item.value === value.value);
+    
+      list.splice(index, 1);
+      localStorage.setItem("todo", JSON.stringify(list));
+}
+
+function add_li(value, checked,flag = 1)
+{       
       
-
-      let delete_button = document.createElement('button');
-      delete_button.textContent = 'Delete';
-      delete_button.className = 'delete';
-
-      span.textContent = value;
-      checkbox.checked = checked;
-      if(checked)
+      
+      var ul = document.getElementById("list"); // lấy thẻ ul
+      
+      var li = document.createElement("li"); // tạo thẻ li
+      var checkbox = document.createElement("input"); // tạo thẻ input
+      checkbox.type = "checkbox"; // tạo checkbox
+      checkbox.id = "checkbox"; // gán id cho thẻ input
+      
+      var span = document.createElement("span"); // tạo thẻ span
+      
+      span.textContent = value; // gán value vào thẻ span
+      const account =
       {
-            li.style.textDecoration = 'line-through';
+            value: value,
+            checked: checked
       }
+      
+      if( flag) // flag = 1 thì thêm vào list // flag =0 thì là update từ local storage
+     { 
+      list.push(account);
+      }
+      localStorage.setItem("todo", JSON.stringify(list));
 
-      span.onclick = function() // update data
-      {
-            span.contentEditable = true;
-            span.focus();
-            span.onblur = function()
-            {
-            fetch("http://localhost:27018/edit",
-                  {
-                        method: 'POST',
-                        body: JSON.stringify({value: value, new_value: span.textContent}),
-                  }
-                  )
+      var deleteButton = document.createElement("button"); // tạo thẻ button
+      deleteButton.textContent = "Delete"; // gán value vào thẻ button
+      deleteButton.className = "delete"; // gán id cho th
+    
+        checkbox.onclick = function() { li.style.textDecoration = checkbox.checked ? "line-through" : "none" 
+            account.checked = checkbox.checked;
+            localStorage.setItem("todo", JSON.stringify(list)); // cập nhật trạng thái checked vào local storage
+        } // tạo sự kiện khi click vào checkbox
+           
+        span.onclick = function() { span.contentEditable = "true" 
+                  account.value = span.textContent;
+            } // tạo sự kiện khi click vào span
             
-            value = span.textContent;
-            }
-      }
+            localStorage.setItem("todo", JSON.stringify(list)); // cập nhật value vào local storage
 
-      checkbox.onclick = function() // update checkbox
-      {
-            fetch("http://localhost:27018/edit",
-            {
-                  method: 'POST',
-                  body: JSON.stringify({ value : value, checked : checkbox.checked, new_value : value}),
-            }
-            )
-            li.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
-            checked = checkbox.checked;
-      }
-      delete_button.onclick = function() //delete
-      {
-            fetch("http://localhost:27018/delete",
-                  {
-                        method: 'POST',
-                        body: JSON.stringify({value:value}),
-                  }
-            )
-            li.remove();
-      }
-      li.appendChild(checkbox);
-      li.appendChild(span);
-      li.appendChild(delete_button);
-      document.getElementById('list').appendChild(li);
+        deleteButton.onclick = function() { li.remove(); delete_list(account)}// tạo sự kiện khi click vào button
+        ul.appendChild(li); // thêm thẻ li vào thẻ ul
+        li.appendChild(checkbox); // thêm thẻ input vào thẻ ul
+        li.appendChild(span); // thêm thẻ span vào thẻ li
+      li.appendChild(deleteButton); // thêm thẻ button vào thẻ ul
+      document.getElementById("input").value="" 
+
+
 }
 
-function api_add(value,checked)
-{
-      fetch("http://localhost:27018/add",
-            {
-                  method: 'POST',
-                  body: JSON.stringify({value : value, checked : checked}),
-            }
-      )
-      log(value,checked);
-   
-     
+
+function press_enter(event) 
+{     
+      let value = document.getElementById("input").value;
+   if(event.key === 'Enter')
+   {  if(value )
+      add_li(value, false);
+   }
 }
 
-function addTodo()
-{     let value = document.getElementById('input').value;
-      let checked = false;
-      if( value )
-      {   api_add(value,checked);
-            }
-      document.getElementById('input').value = '';
+function addTodo() 
+{     let value = document.getElementById("input").value;
+      if (value)
+      add_li(value, false);
 }
-function press_enter(event)
-{
-      if(event.keyCode == 13)
-      {
-            addTodo();
-      }
-}
-
 
 window.onload = function()
 {
-      fetch("http://localhost:27018/get")
-      .then( (response) => response.json() )
-      .then( (data) => 
+      let list = return_todo();
+      console.log(list);
+      for(let i = 0; i < list.length; i++)
       {
-            data.forEach( (element) => 
-            {
-                  log(element.value, element.checked);
-            } 
-            )
-      } 
-      )
+            add_li(list[i].value, list[i].checked,0);
+      }
 }
+
 
 
 
